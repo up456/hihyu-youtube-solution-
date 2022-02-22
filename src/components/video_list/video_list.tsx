eeec428a2a0f08617fad1d05d9ed92626575f13a
Hihyu <4up456@naver.com>
2022-02-22 21:35:26 +0900

    make video item

--- src/components/video_list/video_list.tsx
+++ src/components/video_list/video_list.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import VideoItem from '../video_item/video_item';
+import styles from './video_list.module.css';
 
-const VideoList = (props: any) => (
-  <ul>
-    {props.videos.map((video: any) => (
-      <VideoItem key={video.id} video={video} />
-    ))}
+const VideoList = (props: { videos: any }) => (
+  <ul className={styles.videos}>
+    {props.videos.map(
+      (video: {
+        id: string;
+        snippet: {
+          thumbnails: {
+            medium: {
+              url: string;
+            };
+          };
+          title: string;
+          channelTitle: string;
+        };
+      }) => (
+        <VideoItem key={video.id} video={video} />
+      )
+    )}
   </ul>
 );
 
