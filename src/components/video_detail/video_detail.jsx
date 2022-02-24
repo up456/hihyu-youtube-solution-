b4323fb7665c9dc3eade57b649cd477d25dfd293
Hihyu <4up456@naver.com>
2022-02-24 15:03:00 +0900

    기본 완성

---
+++ src/components/video_detail/video_detail.jsx
@@ -0,0 +1,23 @@
+import styles from './video_detail.module.css';
+
+import React from 'react';
+
+const VideoDetail = ({ video, video: { snippet } }) => (
+  <section className={styles.detail}>
+    <iframe
+      className={styles.video}
+      type="text/html"
+      width="100%"
+      height="500px"
+      src={`https://www.youtube.com/embed/${video.id}`}
+      frameborder="0"
+      allowfullscreen
+      title={video.id}
+    ></iframe>
+    <h2>{snippet.title}</h2>
+    <h2>{snippet.channelTitle}</h2>
+    <pre className={styles.description}>{snippet.description}</pre>
+  </section>
+);
+
+export default VideoDetail;
