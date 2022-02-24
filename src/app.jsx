4c62f3db6b21be71b61eb8800f483d989253fba8
Hihyu <4up456@naver.com>
2022-02-24 15:17:35 +0900

    리팩토링

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
@@ -11,18 +11,21 @@ function App({ youtube }) {
   const selectVideo = (video) => {
     setSelectedVideo(video);
   };
-  const search = (query) => {
-    setSelectedVideo(null);
-    youtube
-      .search(query) //
-      .then((videos) => setVideos(videos));
-  };
+  const search = useCallback(
+    (query) => {
+      setSelectedVideo(null);
+      youtube
+        .search(query) //
+        .then((videos) => setVideos(videos));
+    },
+    [youtube]
+  );
 
   useEffect(() => {
     youtube
       .mostPopular() //
       .then((videos) => setVideos(videos));
-  }, []);
+  }, [youtube]);
 
   return (
     <div className={styles.app}>
--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -10,9 +10,9 @@ const VideoDetail = ({ video, video: { snippet } }) => (
       width="100%"
       height="500px"
       src={`https://www.youtube.com/embed/${video.id}`}
-      frameborder="0"
-      allowfullscreen
-      title={video.id}
+      frameBorder="0"
+      allowFullScreen
+      title="youtube video player"
     ></iframe>
     <h2>{snippet.title}</h2>
     <h2>{snippet.channelTitle}</h2>
